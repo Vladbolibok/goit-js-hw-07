16a14564368e9c82f60430c2e8c40502ced51ef0
Vladislav <vladbolibok@gmail.com>
2025-12-10 07:08:37 +0200

    .

--- js/task-4.js
+++ js/task-4.js
@@ -3,8 +3,8 @@ const formLogin = document.querySelector(".login-form");
 const sendUserInfo = (event) => {
   event.preventDefault();
   const form = event.target;
-  const login = form.elements.email.value.trim;
-  const password = form.elements.password.value.trim;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
   if (!login || !password) {
     alert("All form fields must be filled in");
